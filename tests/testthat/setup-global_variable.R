d80e19ffc0adfa040882679c933044dde8aff01f
Shota Ochi <shotaochi1990@gmail.com>
2019-02-18 14:56:48 +0900

    changed wait time

--- tests/testthat/setup-global_variable.R
+++ tests/testthat/setup-global_variable.R
@@ -1,5 +1,5 @@
 library(magickGUI)
 options("unit_test_magickGUI" = TRUE)
-options("unit_test_magickGUI_wait_time" = 0.1) # sec
+options("unit_test_magickGUI_wait_time" = 0.2) # sec
 options("unit_test_magickGUI_wait_time_long" = 1) # sec
 img <- wizard
