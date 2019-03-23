ad94ca2558a27c79f8e99c80137eef2d59e9ae71
Shota Ochi <shotaochi1990@gmail.com>
2019-03-23 20:59:31 +0900

    confirm whether my understanding is correct or not

--- R/interactive_modulate.R
+++ R/interactive_modulate.R
@@ -109,7 +109,7 @@ interactive_modulate <- function(image, range_max_brightness = 200, range_max_sa
       error = function(e) assign("wait_test", TRUE, inherits = TRUE)
       )
     }
-    for (i in seq(3)) wait_time_long()
+    for (i in seq(10)) wait_time_long()
   }
   tkwm.state(win1, "normal")
   while (TRUE)
