51d9d242b585d18fbca73120147aa15d0f5c51b8
Shota Ochi <shotaochi1990@gmail.com>
2020-06-13 01:43:10 +0900

    restore wait time

--- R/misc.R
+++ R/misc.R
@@ -46,14 +46,14 @@ NULL
 wait_time <- function()
 {
   wait_start <- proc.time()[3]
-  wait_time <- 0.5 # sec
+  wait_time <- 0.1 # sec
   while (proc.time()[3] - wait_start < wait_time) {}
 }
 
 wait_time_long <- function()
 {
   wait_start <- proc.time()[3]
-  wait_time <- 1.0 # sec
+  wait_time <- 0.5 # sec
   while (proc.time()[3] - wait_start < wait_time) {}
 }
 
